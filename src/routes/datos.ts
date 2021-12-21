d476462febe132155f9a5d8dafb27444aeb5388e
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2021-12-21 22:09:20 +0100

    Añadida funcion de insertar en datos

--- src/routes/datos.ts
+++ src/routes/datos.ts
@@ -3,12 +3,11 @@ import { logger, DB_CONFIG, CODIGOS_ESTADO } from '../controller/config';
 import { ErrorMysql } from '../errors/ErrorMysql';
 
 //Importa las funciones de librerias locales
-import { bbdd_a_json, ejecutar_consulta } from "../controller/lib";
-import { respuesta } from '../controller/serv';
+import { bbdd_a_json, ejecutar_consulta, ejecutar_multiples_consultas } from "../controller/lib";
+import { bodyDefinido, respuesta } from '../controller/serv';
 
 //Importa los tipos y funciones de los modulos de node
 import { Router, Request, Response } from "express";
-import { escape } from "mysql";
 
 
 export const router = Router();
@@ -40,7 +39,7 @@ router.get("/:tabla?", async (req: Request, res: Response) => {
         limit = (/^limit\s.*/i.test(order_by) || limit === "") ? limit : `LIMIT ${limit}`;
 
         //LLama a la funcion para ejecutar la consulta, guarda el resultado en una 
-        const json = await ejecutar_consulta(`SELECT * FROM ${req.params.tabla} ${escape(where)} ${escape(group_by)} ${escape(order_by)} ${escape(limit)}`.trim(), DB_CONFIG);
+        const json = await ejecutar_consulta(`SELECT * FROM ${req.params.tabla} ${where} ${group_by} ${order_by} ${limit}`.trim(), DB_CONFIG);
         //Devuelve el resultado de la consulta
         return respuesta(res, json, CODIGOS_ESTADO.OK);
     } catch (e) {
@@ -49,7 +48,7 @@ router.get("/:tabla?", async (req: Request, res: Response) => {
             //En caso de error se almacena en el archivo 
             logger.error_archivo("Error en /:tabla?", {}, <Error>e);
             //Devuelve un mensaje de error, con el codigo 500
-            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Internal_Server_Error);
+            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Bad_Request);
         }
         //Devuelve un mensaje de error indicando un fallo en la consulta
         return respuesta(res, "Fallo al ejecutar la query de busqueda. " + (<Error>e).message, CODIGOS_ESTADO.Internal_Server_Error);
@@ -70,8 +69,72 @@ router.get("/:tabla/formato", async (req: Request, res: Response) => {
         json[`${req.params.tabla}`] = await ejecutar_consulta(`DESCRIBE ${req.params.tabla}`, DB_CONFIG);
 
         //Devuelve el resultado de la consulta
-        return respuesta(res, { '${req.params.tabla}': json }, CODIGOS_ESTADO.OK);
+        return respuesta(res, json, CODIGOS_ESTADO.OK);
     } catch (e) {
+        //Comprueba si el error es una instancia de ErrorMysql o no. En caso de no serlo lo registra en el log
+        if (!(e instanceof ErrorMysql)) {
+            //En caso de error se almacena en el archivo 
+            logger.error_archivo("Error en /:tabla/formato", {}, <Error>e);
+            //Devuelve un mensaje de error, con el codigo 500
+            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Internal_Server_Error);
+        }
+        //Devuelve un mensaje de error indicando un fallo en la consulta
+        return respuesta(res, "Fallo al ejecutar la query de busqueda. " + (<Error>e).message, CODIGOS_ESTADO.Bad_Request);
+    }
+});
+
+//Crear
+router.put("/:tabla?", bodyDefinido, async (req: Request, res: Response) => {
+    try {
+        //Array con las consultas
+        const consultas: Array<string> = [];
+        //En caso de que no se haya especificado una tabla
+        if (req.params.tabla == undefined) {
+            //En caso de que la tabla tokens este declarada
+            delete req.body.Tokens;
+            //Todos las propiedades del cuerpo son tratadas como una tabla y recorridas
+            Object.keys(<Record<string, string>>req.body).forEach((tabla) => {
+                //Crea la plantilla de cada insert
+                let plantilla_consulta = `INSERT INTO ${tabla}`;
+                //Comprueba si la propiedad columnas esta definida. En caso de estarlo añade a la plantilla las columnas sobre las que se realizara el insert
+                if (req.body[tabla].columnas != undefined) plantilla_consulta = `${plantilla_consulta} (${(<Array<string>>req.body[tabla].columnas).join(", ")})`;
+                plantilla_consulta = `${plantilla_consulta} VALUES`;
+                //Recorre los valores de la propiedad valores (array que contiene otro array). 
+                (<Array<Array<string | number>>>req.body[tabla].valores).forEach((valor) => {
+                    //Cada array corresponde con un insert y los valores de este, cada valor del insert
+                    //Añade al array de consultas la consulta
+                    consultas.push(`${plantilla_consulta} ('${valor.join("', '")}')`);
+                });
+            });
+            logger.log_consola(consultas.toString());
+        } else {
+            //Comprueba si la tabla especificada es tokens devuelve una respuesta vacia
+            if (req.params.tabla == "Tokens") return respuesta(res, {}, CODIGOS_ESTADO.Bad_Request);
+             //Crea la plantilla de cada insert
+            let plantilla_consulta = `INSERT INTO ${req.params.tabla}`;
+            //Comprueba si la propiedad columnas esta definida. En caso de estarlo añade a la plantilla las columnas sobre las que se realizara el insert
+            if (req.body.columnas != undefined) plantilla_consulta = `${plantilla_consulta} (${(<Array<string>>req.body.columnas).join(", ")})`;
+            plantilla_consulta = `${plantilla_consulta} VALUES`;
+            //Recorre los valores de la propiedad valores (array que contiene otro array). 
+            (<Array<Array<string | number>>>req.body.valores).forEach((valor) => {
+                //Cada array corresponde con un insert y los valores de este, cada valor del insert
+                //Añade al array de consultas la consulta
+                consultas.push(`${plantilla_consulta} ('${valor.join("', '")}')`);
+            });
+            logger.log_consola(`2 ${consultas.toString()}`);
+        }
+
+        //Ejecuta el erray de consultas
+        ejecutar_multiples_consultas(consultas, DB_CONFIG);
+
+        //Devulve un mensaje indicando que todo ha ido bien
+        respuesta(res, "Datos creados correctamente correcta", CODIGOS_ESTADO.OK);
+    } catch (e) {
+        //En caso de que algun valor de la petición sea incorrecto, no sea del tipo esperado
+        if (e instanceof TypeError) {
+            //Devuelve un menaje indicando el error, y ejemplos de uso
+            return respuesta(res, { error: "Alguna propiedad del cuerpo es incorrecto", info: `${req.originalUrl}/info` }, CODIGOS_ESTADO.Bad_Request);
+        }
         //Comprueba si el error es una instancia de ErrorMysql o no. En caso de no serlo lo registra en el log
         if (!(e instanceof ErrorMysql)) {
             //En caso de error se almacena en el archivo 
