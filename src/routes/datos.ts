cded3384bf4818e0f08e73df441fa87f58076a9b
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2021-12-27 14:44:45 +0100

    Añadida funcion de actualizar la base de datos

--- src/routes/datos.ts
+++ src/routes/datos.ts
@@ -48,7 +48,7 @@ router.get("/:tabla?", async (req: Request, res: Response) => {
             //En caso de error se almacena en el archivo 
             logger.error_archivo("Error en /:tabla?", {}, <Error>e);
             //Devuelve un mensaje de error, con el codigo 500
-            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Bad_Request);
+            return respuesta(res, "Error del servidor", (<ErrorMysql>e).codigo);
         }
         //Devuelve un mensaje de error indicando un fallo en la consulta
         return respuesta(res, "Fallo al ejecutar la query de busqueda. " + (<Error>e).message, CODIGOS_ESTADO.Internal_Server_Error);
@@ -76,14 +76,13 @@ router.get("/:tabla/formato", async (req: Request, res: Response) => {
             //En caso de error se almacena en el archivo 
             logger.error_archivo("Error en /:tabla/formato", {}, <Error>e);
             //Devuelve un mensaje de error, con el codigo 500
-            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Internal_Server_Error);
+            return respuesta(res, "Error del servidor", (<ErrorMysql>e).codigo);
         }
         //Devuelve un mensaje de error indicando un fallo en la consulta
         return respuesta(res, "Fallo al ejecutar la query de busqueda. " + (<Error>e).message, CODIGOS_ESTADO.Bad_Request);
     }
 });
 
-//Crear
 router.put("/:tabla?", bodyDefinido, async (req: Request, res: Response) => {
     try {
         //Array con las consultas
@@ -106,11 +105,10 @@ router.put("/:tabla?", bodyDefinido, async (req: Request, res: Response) => {
                     consultas.push(`${plantilla_consulta} ('${valor.join("', '")}')`);
                 });
             });
-            logger.log_consola(consultas.toString());
         } else {
             //Comprueba si la tabla especificada es tokens devuelve una respuesta vacia
             if (req.params.tabla == "Tokens") return respuesta(res, {}, CODIGOS_ESTADO.Bad_Request);
-             //Crea la plantilla de cada insert
+            //Crea la plantilla de cada insert
             let plantilla_consulta = `INSERT INTO ${req.params.tabla}`;
             //Comprueba si la propiedad columnas esta definida. En caso de estarlo añade a la plantilla las columnas sobre las que se realizara el insert
             if (req.body.columnas != undefined) plantilla_consulta = `${plantilla_consulta} (${(<Array<string>>req.body.columnas).join(", ")})`;
@@ -121,14 +119,13 @@ router.put("/:tabla?", bodyDefinido, async (req: Request, res: Response) => {
                 //Añade al array de consultas la consulta
                 consultas.push(`${plantilla_consulta} ('${valor.join("', '")}')`);
             });
-            logger.log_consola(`2 ${consultas.toString()}`);
         }
 
-        //Ejecuta el erray de consultas
+        //Ejecuta el array de consultas
         ejecutar_multiples_consultas(consultas, DB_CONFIG);
 
         //Devulve un mensaje indicando que todo ha ido bien
-        respuesta(res, "Datos creados correctamente correcta", CODIGOS_ESTADO.OK);
+        return respuesta(res, "Datos creados correctamente correcta", CODIGOS_ESTADO.OK);
     } catch (e) {
         //En caso de que algun valor de la petición sea incorrecto, no sea del tipo esperado
         if (e instanceof TypeError) {
@@ -143,6 +140,109 @@ router.put("/:tabla?", bodyDefinido, async (req: Request, res: Response) => {
             return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Internal_Server_Error);
         }
         //Devuelve un mensaje de error indicando un fallo en la consulta
-        return respuesta(res, "Fallo al ejecutar la query de busqueda. " + (<Error>e).message, CODIGOS_ESTADO.Bad_Request);
+        return respuesta(res, "Fallo al insertar los datos. " + (<Error>e).message, CODIGOS_ESTADO.Bad_Request);
+    }
+});
+
+//----------------------------------------------------------------------------------
+router.post("/:tabla?", async (req: Request, res: Response): Promise<Response> => {
+    try {
+        //Array con las consultas
+        const consultas: Array<string> = [];
+        //En caso de que el body no sea un array, crea un array del body
+        if (!Array.isArray(req.body)) req.body = [req.body];
+        //En caso de que no se haya especificado una tabla
+        if (req.params.tabla == undefined) {
+            //El cuerpo se trata como un array de objetos. En caso de no serlo se lanzaria un error. Cada objecto se considera que tiene unas opciones especificas
+            req.body.forEach((query: { tabla: string, where: string, order_by?: string, limit?: string, valores: Record<string, string> }) => {
+                //En caso de que tabla o el where sean indefinidos lanzará un ErrorMysql
+                if (query['tabla'] === undefined || query['where'] === undefined || query['where'].trim() === "") throw new ErrorMysql("Error", CODIGOS_ESTADO.Bad_Request);
+                //Si la tabla es tokens se ignora el objeto
+                if (query['tabla'] == "Tokens") return;
+                //Crea la plantilla de cada insert  
+                let plantilla_consulta = `UPDATE ${query['tabla']} SET`;
+                //Recorre el objeto valores. 
+                Object.keys(query['valores']).forEach((valor) => {
+                    //Las claves del objeto corresponden con la columna. Y su valor con el nuevo valor de esta
+                    plantilla_consulta = `${plantilla_consulta} ${valor} = ${(query['valores'])[valor]}, `;
+                });
+
+                //Elimina la ultima coma y añade un espacio
+                plantilla_consulta = plantilla_consulta.slice(0, -2).concat(" ");
+
+                //Obtiene los valores de los condicionales. Salvo where, si no estan especificados
+                //se especifica un valor vacio
+                let where: string = query['where'].toString();
+                let order_by: string = query['order_by']?.toString() || "";
+                let limit: string = query['limit']?.toString() || "";
+
+                //Comprueba si los condicionales tiene su respectivo condicional o ha de añadirlo. Si el 
+                //valor es vacio no añade el condicional
+                where = (/^where\s.*/.test(where) || where === "") ? where : `WHERE ${where}`;
+                order_by = (/^order\sby\s.*/i.test(order_by) || order_by === "") ? order_by : `ORDER BY ${order_by}`;
+                limit = (/^limit\s.*/i.test(order_by) || limit === "") ? limit : `LIMIT ${limit}`;
+
+                //Añade la consulta al array de consultas
+                consultas.push(`${plantilla_consulta} ${where} ${order_by} ${limit}`.trim());
+            });
+        } else {
+            //Comprueba si la tabla especificada es tokens devuelve una respuesta vacia
+            if (req.params.tabla == "Tokens") return respuesta(res, {}, CODIGOS_ESTADO.Bad_Request);
+            //Guarda la tabla en una variable.
+            const tabla = req.params.tabla;
+            //El cuerpo se trata como un array de objetos. En caso de no serlo se lanzaria un error
+            req.body.forEach((query: { where: string, order_by?: string, limit?: string, valores: Record<string, string> }) => {
+                //En caso de que tabla o el where sean indefinidos lanzará un ErrorMysql
+                if (query['where'] === undefined || query['where'].trim() === "") throw new ErrorMysql("Error", CODIGOS_ESTADO.Bad_Request);
+                //Crea la plantilla de cada insert  
+                let plantilla_consulta = `UPDATE ${tabla} SET`;
+                //Recorre el objeto valores. 
+                Object.keys(query['valores']).forEach((valor) => {
+                    //Las claves del objeto corresponden con la columna. Y su valor con el nuevo valor de esta
+                    plantilla_consulta = `${plantilla_consulta} ${valor} = ${(query['valores'])[valor]}, `;
+                });
+
+                //Elimina la ultima coma y añade un espacio
+                plantilla_consulta = plantilla_consulta.slice(0, -2).concat(" ");
+
+                //Obtiene los valores de los condicionales. Salvo where, si no estan especificados
+                //se especifica un valor vacio
+                let where: string = query['where'].toString();
+                let order_by: string = query['order_by']?.toString() || "";
+                let limit: string = query['limit']?.toString() || "";
+
+                //Comprueba si los condicionales tiene su respectivo condicional o ha de añadirlo. Si el 
+                //valor es vacio no añade el condicional
+                where = (/^where\s.*/.test(where) || where === "") ? where : `WHERE ${where}`;
+                order_by = (/^order\sby\s.*/i.test(order_by) || order_by === "") ? order_by : `ORDER BY ${order_by}`;
+                limit = (/^limit\s.*/i.test(order_by) || limit === "") ? limit : `LIMIT ${limit}`;
+
+                //Añade la consulta al array de consultas
+                consultas.push(`${plantilla_consulta} ${where} ${order_by} ${limit}`.trim());
+            });
+        }
+
+        //Ejecuta el array de consultas
+        ejecutar_multiples_consultas(consultas, DB_CONFIG);
+
+        //Devulve un mensaje indicando que todo ha ido bien
+        return respuesta(res, "Datos actualizados correctamente correcta", CODIGOS_ESTADO.OK);
+    } catch (e) {
+        //En caso de que algun vlor de la petición sea incorrecto, no sea del tipo esperado
+        if (e instanceof TypeError) {
+            //Devuelve un menaje indicando el error, y ejemplos de uso
+            return respuesta(res, { error: "Alguna propiedad del cuerpo es incorrecto", info: `${req.originalUrl}/info` }, CODIGOS_ESTADO.Bad_Request);
+        }
+        //Comprueba si el error es una instancia de ErrorMysql o no. En caso de no serlo lo registra en el log
+        if (!(e instanceof ErrorMysql)) {
+            //En caso de error se almacena en el archivo 
+            logger.error_archivo("Error en /:tabla/formato", {}, <Error>e);
+            //Devuelve un mensaje de error, con el codigo 500
+            return respuesta(res, "Error del servidor", CODIGOS_ESTADO.Internal_Server_Error);
+        }
+        logger.log_consola("Algo ha fallado", {}, e);
+        console.log(e.name);
+        //Devuelve un mensaje de error indicando un fallo en la consulta
+        return respuesta(res, "Fallo al actualizar los datos. " + (<Error>e).message, CODIGOS_ESTADO.Bad_Request);
     }
 });
