fbb9a73aa5c2d7d89acdec78a4b62fc99ca890c0
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2021-12-28 00:22:10 +0100

    Modificado config.ts

--- src/controller/config.ts
+++ src/controller/config.ts
@@ -5,6 +5,30 @@
 import { ConnectionConfig } from "mysql";
 import { Logger } from "logger";
 
+declare global {
+    interface String {
+        /**
+         * Permite definir una plantilla como string y compilarla al llamar a este metodo
+         * @param args objeto, con un string como clave, y cualquier tipo como valor
+         * @returns una Funcion que cambia sustitulle los valores en la plantill
+         * @see Codigo de {@link https://stackoverflow.com/questions/29182244/convert-a-string-to-a-template-string/41015840#41015840 StackOverflow}
+         */
+
+        compilarPlantilla(args: Record<string, unknown>): Function;
+    }
+}
+
+String.prototype.compilarPlantilla = function (this: string, args: Record<string, unknown>): Function {
+    //Extrae los nombres de la funcion
+    const nombres: string[] = Object.keys(args);
+    //Extrae los valores
+    const valores: unknown[] = Object.values(args);
+    //Devuelve una funcion que tiene por parametros todos los nombres de la funcion y devuelve la plantilla con los valores sustituidos
+    //Se le pasan por parametro todos los valores, al haberse extraido de un objeto cada valor corresponde con su clave o nombre
+    return new Function(...nombres, `return \`${this}\``)(...valores);
+};
+
+
 //Exporta la variable logger para el manejo de los logs
 export const logger = new Logger();
 
@@ -41,3 +65,4 @@ export enum CODIGOS_ESTADO {
     Unauthorized = 401,
     Internal_Server_Error = 500
 }
+
